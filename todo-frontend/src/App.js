9dba5d210cf5f5772e526bc1e0224856abf8dd10
MuteeVincente <vinniemutee@gmail.com>
2023-08-22 00:13:41 +0300

    adjusting styled-components

--- todo-frontend/src/App.js
+++ todo-frontend/src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-
+import Todo from './components/Todo';
 function App() {
   return (
     <div className="App">
       <h1>Todo App</h1>
-
+      <Todo name = 'Vincent'/>
     </div>
   );
 }
---
+++ todo-frontend/src/components/Todo/index.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {Container} from './styles';
+
+function Todo() {
+  return (
+    <Container>Todo Component</Container>
+  )
+}
+
+export default Todo
