c4d0acdd9b9daeade571c6d55a885f59c3c0b562
MuteeVincente <vinniemutee@gmail.com>
2023-08-22 07:34:15 +0300

    fetching data from api

--- todo-backend/server.js
+++ todo-backend/server.js
@@ -5,6 +5,14 @@ const dotenv = require('dotenv');
 
 dotenv.config();
 
+const{
+    getTodo,
+    createTodo,
+    updateTodo,
+    deleteTodo
+
+} = require('./controllers/todoController')
+
 // App config
 const app = express();
 
@@ -22,9 +30,25 @@ mongoose.connect(connectionURL)
 .then(() => {
     console.log('Connected to MongoDB');
     app.listen(port, () => {
-        console.log('Running on port: ${port}');
+        console.log(`Running on port: ${port}`);
     });
 })
 .catch(error => {
     console.error('Error connecting to MongoDB:', error);
 });
+
+//Create Endpoints
+
+
+//Get todos list
+
+app.get('/todos', getTodo)
+
+// Create  a new Todo
+app.post('/todos', createTodo)
+
+//Update a Todo
+app.put('/todos/:id', updateTodo)
+
+//Delete a Todo
+app.delete('/todos/:id', deleteTodo)
--- todo-frontend/src/components/Todo/index.js
+++ todo-frontend/src/components/Todo/index.js
@@ -1,10 +1,30 @@
-import React ,{useState} from 'react';
+import React ,{useEffect, useState} from 'react';
 import {Container} from './styles';
 import Form from  '../Form';
+import axios from '../../axios'
 function Todo() {
   const [input, setInput] = useState('');
-  console.log(input, 'input')
+  const [todos, setTodos] = useState([]);
+  
+  // console.log(input, 'input')
 
+  const  fetchData = async () => {
+    try{
+      const response = await axios.get('/todos')
+      setTodos(response.data);
+    }catch(err){
+      console.log(err.message)
+    }
+  }
+
+  useEffect(() => {
+    fetchData()
+  }, [])
+
+  const addTodo =  async () => {
+    console.log('Todo Added')
+  }
+  console.log(todos, 'todos')
   return (
     <Container>
       <h2>List of Todos </h2>
